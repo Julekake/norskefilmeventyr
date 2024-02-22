db5c6ceb75cde52160ef935ce471dbeee806b9c5
Julekake <160511721+Julekake@users.noreply.github.com>
2024-02-22 23:16:38 +0100

    norskefilmeventyr

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const aboutButton = document.getElementById("about-button");
 const contactButton = document.getElementById("contact-button");
 
-aboutButtonaddEventListener("click", () => {
-    // Show the "Om oss" section and hide the others
+contactButton.addEventListener("click"), () => {
+    
+}    // Show the "Om oss" section and hide the others
     const sections = document.getElementsByTagName("section");
     for (const section of sections) {
         if (section.textContent.includes("Om oss")) {
@@ -13,7 +14,7 @@ aboutButtonaddEventListener("click", () => {
     }
 });
 
-contactButtonaddEventListener("click", () => {
+contactButton.addEventListener("click", () => {
     // Show the "Kontakt oss" section and hide the others
     const sections = document.getElementsByTagName("section");
     for (const section of sections) {
